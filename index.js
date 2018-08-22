9aebb2c5aea165cf1f84b530e630c9b327100788
azhry <arliansyah_azhary@yahoo.com>
2018-08-22 21:20:51 +0700

    Added server crash simulation to test availability

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const pid = process.pid;
+
+http.createServer((req, res) => {
+	for (let i = 0; i < 1e7; i++) {
+		res.end('Handled by process', pid);
+	}
+}).listen(3000, () => {
+	console.log('Started process', pid);
+	setTimeout(() => {
+		process.exit(1) // death by random timeout
+	}, Math.random() * 10000);
+});
